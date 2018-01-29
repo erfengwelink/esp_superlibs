b04d7f3168e84102a42dab67bcdc805c03374ba9
erfengwelink <erfeng@welink.io>
2018-01-29 19:06:06 +0800

    add wifi test block

--- main/main.c
+++ main/main.c
@@ -1,13 +1,23 @@
 #include "app_can.h"
+#include "app_wifi.h"
+#include "nvs_flash.h"
+
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "freertos/queue.h"
 
 void app_main()
 {
-    app_can_setup();
-    xTaskCreate(CAN_poll_task,"CAN_poll_task",2048, NULL, 5, NULL);
+    // Initialize NVS
+    esp_err_t ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES) {
+        ESP_ERROR_CHECK(nvs_flash_erase());
+        ret = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK( ret );
+    //app_can_setup();
+    //xTaskCreate(CAN_poll_task,"CAN_poll_task",2048, NULL, 5, NULL);
     //app_can_send(...); /* "..." depends on your applications.*/
-
+    initialise_wifi();
 }
 
